7c5a1c23cabe0b05ca2969a761249a77dc2792d6
Oleksandr Kovalov <oleksandr.k19251@volpis.com>
2024-07-27 11:22:22 +0300

    test

--- Sources/Templates/Menu/Menu+SwiftUI.swift
+++ Sources/Templates/Menu/Menu+SwiftUI.swift
@@ -54,7 +54,34 @@ public extension Templates {
                 label(fadeLabel)
                     .frameTag(model.id)
                     .contentShape(Rectangle())
-                    .onTap(gestureModel: gestureModel, fadeLabel: $fadeLabel, model: model, window: window)
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                            .onChanged { value in
+
+                                gestureModel.onDragChanged(
+                                    newDragLocation: value.location,
+                                    model: model,
+                                    labelFrame: window.frameTagged(model.id),
+                                    window: window
+                                ) { present in
+                                    model.present = present
+                                } fadeLabel: { fade in
+                                    fadeLabel = fade
+                                }
+                            }
+                            .onEnded { value in
+                                gestureModel.onDragEnded(
+                                    newDragLocation: value.location,
+                                    model: model,
+                                    labelFrame: window.frameTagged(model.id),
+                                    window: window
+                                ) { present in
+                                    model.present = present
+                                } fadeLabel: { fade in
+                                    fadeLabel = fade
+                                }
+                            }
+                    )
                     .onValueChange(of: model.present) { _, present in
                         if !present {
                             withAnimation(model.configuration.labelFadeAnimation) {
@@ -101,34 +128,5 @@ public extension Templates {
             }
         }
     }
-}
-
-extension View {
-    @ViewBuilder
-    func onTap(
-        gestureModel: Templates.MenuGestureModel,
-        fadeLabel: Binding<Bool>,
-        model: Templates.MenuModel,
-        window: UIWindow?
-    ) -> some View {
-            if #available(iOS 17, *) {
-                self
-                .onTapGesture { location in
-                    gestureModel.onDragEnded(
-                        newDragLocation: location,
-                        model: model,
-                        labelFrame: window.frameTagged(model.id),
-                        window: window
-                    ) { present in
-                        model.present = present
-                    } fadeLabel: { fade in
-                        fadeLabel.wrappedValue = fade
-                    }
-                }
-            }
-            else {
-                self
-            }
-        }
 }
 #endif
